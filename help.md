d0ae7fb23db8481193400dabcf87a04d47a5ed66
Zan <plahutazan3@gmail.com>
2026-01-03 18:17:58 +0100

    initial commit

---
+++ help.md
@@ -0,0 +1,18 @@
+# CAMERAS
+
+### Host name
+        dashcam1/2
+
+### Username
+        user
+
+### Password
+        wasd
+
+### SSH
+        ssh user@dashcam1.local
+
+### Recording
+        nano record_video.sh
+        chmod +x record_video.sh
+        ./record_video.sh
---
+++ record_video.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+SAVE_DIR="$HOME/videos"
+SEGMENT_MS=120000          # 2 minutes
+MAX_AGE_MINUTES=240       # 4 hours
+
+mkdir -p "$SAVE_DIR"
+
+echo "Starting continuous segmented recording..."
+
+rpicam-vid \
+  -t 0 \
+  --segment $SEGMENT_MS \
+  --codec h264 \
+  --inline \
+  --width 1920 \
+  --height 1080 \
+  --framerate 30 \
+  --bitrate 8000000 \
+  --awb auto \
+  --exposure normal \
+  -o "$SAVE_DIR/video_%04d.h264" &
+
+RPICAM_PID=$!
+
+# Cleanup loop (timestamp-free but works)
+while kill -0 $RPICAM_PID 2>/dev/null; do
+    find "$SAVE_DIR" -type f -name "*.h264" -mmin +$MAX_AGE_MINUTES -delete
+    sleep 60
+done
