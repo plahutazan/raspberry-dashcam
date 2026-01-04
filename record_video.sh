345aca7b7a3f86c2cce3af4007ca302dafdc45a0
Zan <plahutazan3@gmail.com>
2026-01-04 21:36:25 +0100

    fix date deletion, overwriting

--- record_video.sh
+++ record_video.sh
@@ -2,11 +2,14 @@
 
 SAVE_DIR="$HOME/videos"
 SEGMENT_MS=120000          # 2 minutes
-MAX_AGE_MINUTES=240       # 4 hours
+MAX_AGE_MINUTES=240        # 4 hours
 
 mkdir -p "$SAVE_DIR"
 
-echo "Starting continuous segmented recording..."
+# Date/time for this recording session
+SESSION_DATE=$(date +"%Y-%m-%d_%H-%M-%S")
+
+echo "Starting continuous segmented recording ($SESSION_DATE)..."
 
 rpicam-vid \
   -t 0 \
@@ -19,11 +22,11 @@ rpicam-vid \
   --bitrate 8000000 \
   --awb auto \
   --exposure normal \
-  -o "$SAVE_DIR/video_%04d.h264" &
+  -o "$SAVE_DIR/video_${SESSION_DATE}_%04d.h264" &
 
 RPICAM_PID=$!
 
-# Cleanup loop (timestamp-free but works)
+# Cleanup loop
 while kill -0 $RPICAM_PID 2>/dev/null; do
     find "$SAVE_DIR" -type f -name "*.h264" -mmin +$MAX_AGE_MINUTES -delete
     sleep 60
